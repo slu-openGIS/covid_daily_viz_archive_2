3c8465b502a4837148a74d4de0df2f67fcd2e5a3
Christopher Prener <chris.prener@slu.edu>
2021-10-06 13:55:38 -0500

    build am plots for 2021-10-06

--- source/_plot_am.R
+++ source/_plot_am.R
@@ -45,7 +45,7 @@ values <- list(
   county_log_y = .4,
   
   regional_current_x = -360,
-  state_current_y = 1700,
+  state_current_y = 1800,
   
   caption_text = "Plot by Christopher Prener, Ph.D.\nData via the New York Times COVID-19 Project",
   caption_text_census = "Plot by Christopher Prener, Ph.D.\nData via the New York Times COVID-19 Project and the U.S. Census Bureau",
