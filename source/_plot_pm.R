e7129cae4b09958cb2922c5b41908da1327e893c
Christopher Prener <chris.prener@slu.edu>
2021-10-28 14:04:56 -0500

    build am plots for 2021-10-28

--- source/_plot_pm.R
+++ source/_plot_pm.R
@@ -64,7 +64,7 @@ source("source/workflow/03_state_individual_plots.R")
 
 # regional plots
 # source("source/workflow/18_stl_zip_plots_v3.R")
-# # source("source/workflow/19_stl_individual_plots.R")
+source("source/workflow/19_stl_individual_plots.R")
 
 if (weekdays(date) %in% c("Saturday", "Sunday") == FALSE){
   source("source/workflow/20_stl_hospital_plots.R")  
